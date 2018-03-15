a7aa7800eb1dce24651aee0b654ece480d999978
Hephaaistos <36539189+Hephaaistos@users.noreply.github.com>
2018-03-15 11:46:30 +0100

    Update GemTDShop.py
    New shop ID, new 12h sale

--- GemTDShop.py
+++ GemTDShop.py
@@ -2,28 +2,27 @@ import urllib.request
 from bs4 import BeautifulSoup
 import json
 from datetime import datetime, timedelta
-
+import time
 from shop_index import *
 
-SHOP_URL = "http://101.200.189.65:430/gemtd/goods/list/v2/@0"  # @steam ID lets you check for your id
+SHOP_URL = "http://101.200.189.65:430/gemtd/201803/goods/list/@76561198047094522?hehe=0.2642897393088788"  # @steam ID lets you check for your id
 # url = "http://101.200.189.65:430/gemtd/goods/list/v1/@0"  # Old Shop URL
 # url = "http://101.200.189.65:430/gemtd/goods/list/v1?hehe=0.3792814633343369"
 
+dtnow = datetime.utcnow()
 
-def calculate_time_until_refresh():
-    today = datetime.utcnow()
+dt22 = None
+if dtnow.hour < 4:
+    dt22 = datetime(dtnow.year, dtnow.month, dtnow.day, 4, 0, 0, 0)
+elif dtnow.hour < 16:
+    dt22 = datetime(dtnow.year, dtnow.month, dtnow.day, 16, 0, 0, 0)
+else:
+    tmrw = dtnow + timedelta(days=1)
+    dt22 = datetime(tmrw.year, tmrw.month, tmrw.day, 4, 0, 0, 0)
 
-    if today.hour < 21:
-        update_time = datetime(today.year, today.month, today.day, 21, 0, 0, 0)
-    else:
-        tomorrow = today + timedelta(days=1)
-        update_time = datetime(tomorrow.year, tomorrow.month, tomorrow.day, 21, 0, 0, 0)
+diff=dt22-dtnow
 
-    diff = update_time-today
-
-    return divmod(diff.seconds,3600)[0]
-
-def format_title(sale_id=None, time_to_refresh="??"):
+def format_title(sale_id=None):
     """
     This function creates a string containing the date of today, the sale item and its type (Hero, Ability, Effect)
     Useful for title of an reddit thread.
@@ -35,13 +34,8 @@ def format_title(sale_id=None, time_to_refresh="??"):
     sale_name = COMBINED[sale_id]
     # The type is indicated by the first character of the id
     sale_type = TYPE[sale_id[0]]
-
-    # Return the formated title
     return "    [Shop] {0} ({2} on Sale: {1}) [{3}h till refresh]"\
-        .format(datetime.today().strftime('%d-%m-%Y'),
-                sale_name,
-                sale_type,
-                time_to_refresh)
+        .format(datetime.today().strftime('%d-%m-%Y'), sale_name, sale_type, divmod(diff.seconds,3600)[0])
 
 
 def format_item_output(item: dict, sale_id=None):
@@ -60,7 +54,6 @@ def format_item_output(item: dict, sale_id=None):
               "    --------                   ----------------\n"
 
     return out.format(TYPE[item_id[0]], COMBINED[item_id], price, rarity)
-
 
 def format_lines_of_shop(shop_elements, sale_id):
     for k, v in shop_elements.items():
@@ -84,25 +77,20 @@ def main():
 
     # break into lines and remove leading and trailing space on each
     lines = (line.strip() for line in text.splitlines())
-
     # break multi-headlines into a line each
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-
     # drop blank lines
     text = '\n'.join(chunk for chunk in chunks if chunk)
-
     # create json string from text of website
     json_str = '{0}'.format(text)
-
     # create dict from json
     obj = json.loads(json_str)
 
     # read out the shop items(dict) and the sale ID(str)
     shop_elements = obj['list']
     sale_id = obj['onsale']
-    time_to_refresh = calculate_time_until_refresh()
 
-    print(format_title(sale_id, time_to_refresh))
+    print(format_title(sale_id))
     print("    Today's Heroes and Abilities are:")
     print(*format_lines_of_shop(shop_elements, sale_id), sep='')
 
@@ -110,4 +98,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
